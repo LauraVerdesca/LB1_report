05de95caa16aedf11b735112caf0f582979596c9
LauraVerdesca <106587834+LauraVerdesca@users.noreply.github.com>
2024-05-20 10:11:45 +0200

    Add files via upload

---
+++ select_fasta.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import numpy as np
+
+def get_seqs(fileseq, pos=0):
+    dseq = {}
+    f = open(fileseq)
+    for line in f:
+        if line[0] == '>':
+            k = line[1:].rstrip().split('|')[pos]
+            dseq[k] = ''
+            continue
+        dseq[k] = dseq[k] + line.rstrip()
+    return dseq
+
+
+if __name__ == '__main__':
+    fileids = sys.argv[1]
+    fileseq = sys.argv[2]
+    pos=0
+    if len(sys.argv) > 3:
+        pos = int(sys.argv[3])-1 # if there is a third input, consider it as an integer
+    dseq = get_seqs(fileseq, pos)
+    ids = open(fileids).read().rstrip().split('\n')
+    for i in ids:
+        if dseq.get(i, 0) != 0:
+            print('>' + i + '\n' + dseq[i])
+            
+ 
